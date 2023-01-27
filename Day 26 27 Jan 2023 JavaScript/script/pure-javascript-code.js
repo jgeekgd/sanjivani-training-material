f9d50a743e5a5bef02d6e145c1431cfba3f9246c
igd-revature <gangadhar.parde@revature.com>
2023-01-27 12:11:04 +0530

    Day 25

---
+++ Day 26 27 Jan 2023 JavaScript/script/pure-javascript-code.js
@@ -0,0 +1,7 @@
+function hello(greeting) {
+  console.log(greeting + " Accepted!");
+}
+hello("GD");
+global.setInterval(function () {
+  console.log("Hello\n");
+}, 1000);
---
+++ Day 26 27 Jan 2023 JavaScript/script/script.js
@@ -0,0 +1,44 @@
+/*
+document is the root dom object whcih we can access directly inside javascript.
+
+*/
+
+console.log(window);
+
+function hello(greeting) {
+  console.log(greeting + " Accepted!");
+}
+
+window.hello("GD");
+
+document.write("1</br>");
+document.write("2</br>");
+document.write("3</br>");
+document.write("4</br>");
+document.write("5</br>");
+document.write("6</br>");
+document.write("7</br>");
+document.write("8</br>");
+document.write("9</br>");
+document.write("10</br>");
+document.write("11</br>");
+document.write("12</br>");
+document.write("13</br>");
+document.write("14</br>");
+document.write("15</br>");
+document.write("16</br>");
+document.write("17</br>");
+document.write("18</br>");
+document.write("19</br>");
+document.write("20</br>");
+document.write("21</br>");
+document.write("22</br>");
+document.write("23</br>");
+document.write("24</br>");
+document.write("25</br>");
+/*
+using console.log we can log something in browser console.
+this will not be visible in DOM
+*/
+
+console.log("===========Check Browser Console=====================");
